45912dfb0bf631a0e10c953f453a52d097ca4b89
johannes60-sk <garilsenan@gmail.com>
2024-06-27 16:33:18 +0200

    initialize project

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import static org.example.RomanNumerals.toRoman;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println(toRoman(2));
+    }
+}
---
+++ src/main/java/org/example/RomanNumerals.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class RomanNumerals {
+    public static String toRoman(int num) {
+        String[] romanNumerals = new String[] {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        int[] values = new int[] {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < values.length; i++) {
+            while (num >= values[i]) {
+                result.append(romanNumerals[i]);
+                num -= values[i];
+            }
+        }
+        return result.toString();
+    }
+}
