e1f3a795feafe3462d89a672f88c232063bd247b
johannes60-sk <garilsenan@gmail.com>
2024-07-01 16:04:26 +0200

    test failed

--- src/test/java/RomanNumeralsTest.java
+++ src/test/java/RomanNumeralsTest.java
@@ -1,10 +1,15 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.example.RomanNumerals;
 import org.junit.jupiter.api.Test;
+
 
 public class RomanNumeralsTest {
 
     @Test
     public void testConvert1ToI() {
-        assertEquals("I", RomanNumerals.toRoman(1));
+        assertEquals("I", RomanNumerals.convertToRoman(1));
+        assertEquals("II", RomanNumerals.convertToRoman(2));
+
     }
 }
